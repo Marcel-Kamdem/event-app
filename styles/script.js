b30706a38ce2c394f3a2808153fbb5c8aeb4ef22
Jonh-jonhy <jonathankamdem000@gmail.com>
2025-12-22 19:57:26 +0100

    ajout de la nav bar

--- styles/script.js
+++ styles/script.js
@@ -1,3 +1,22 @@
+//Components definitions
+
+//Toggle button
+/*
+* Bouton de switch entre le mode sombre et le mode claire
+**/ 
+class TOGButtun extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <div class="toggle-btn">
+            <input type="checkbox" id="check" checked>
+            <label for="check" class="btn"></label>
+        </div>
+        `
+    }
+}
+customElements.define("tog-buttun", TOGButtun);
+
+//Fonctionnalite du toggle buttun
 let toggle_btn = document.getElementById("check");
 let body = document.querySelector("body");
 
@@ -10,3 +29,23 @@ toggle_btn.addEventListener("click", () => {
          body.classList.add("dark");
     }
 });
+
+//Component Nav-bar
+class TNav extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <nav class="nav-bar">
+            <div class="logo"><a href="#">Horairo</a></div>
+            <div class="nav-link">
+                <a href="#" class="login">
+                    <div class="bouton">login</div>
+                </a>
+                <a href="#" class="signUp">
+                    <div class="bouton">sign up</div>
+                </a>
+            </div>
+        </nav>
+        `
+    }
+}
+customElements.define("t-nav", TNav)
